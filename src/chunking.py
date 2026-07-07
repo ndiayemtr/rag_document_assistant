12cb15da5bbaa9a849eaecc1b2becb01ec7dbdd3
matar <matar.ndiaye@akilee.tech>
2026-07-07 16:04:48 +0000

    decouper par chunk

---
+++ src/chunking.py
@@ -0,0 +1,20 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+
+class TextChunker:
+
+    def __init__(
+        self,
+        chunk_size=1000,
+        chunk_overlap=200
+    ):
+
+        self.splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap
+        )
+
+    def split(self, text):
+
+        return self.splitter.split_text(text)
--- test_loader.py
+++ test_loader.py
@@ -1,8 +1,18 @@
 from src.loader import PDFLoader
+from src.chunking import TextChunker
 from src.config import PDF_DIR
+
 
 loader = PDFLoader(PDF_DIR / "rapport.pdf")
 
 text = loader.load()
 
-print(text[:100])
+chunker = TextChunker()
+
+chunks = chunker.split(text)
+
+print("Nombre de chunks:", len(chunks))
+
+print("\nPremier chunk :\n")
+
+print(chunks[0])
